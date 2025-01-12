97f593fa961178b86fb6aae7ff5d1aa6d36eca39
marcos <marcos23456@discente.ufg.br>
2025-01-12 14:30:59 -0300

    last commit for navbar

--- 3D-Portfolio/src/components/Navbar.jsx
+++ 3D-Portfolio/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ onClick={()=>{
   window.scrollTo(0,0)
 }}>
 <img src={logo} alt="logo" className='w-9 h-9 object-contain'  />
-<p className='text-white text-[18px] font-bold cursor-pointer decoration-none'>Marcos Martins <span className='sm:block hidden'>| Web Developer</span></p>
+<p className='text-white text-[18px] font-bold cursor-pointer flex'>Marcos Martins &nbsp; <span className='sm:block hidden'>| Web Developer</span></p>
 </Link>
 <ul className='list-none hidden sm:flex flex-row gap-10 '>
 
